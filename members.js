91527b8742eefd182960712af4e5582e75513d45
wrrui <97753526+wrrui@users.noreply.github.com>
2025-09-04 20:56:43 +0300

    Update members.js

--- members.js
+++ members.js
@@ -2,8 +2,7 @@
 // List of all members with id, password, name, and role
 const membersData = [
     { id: "id-120-444-55", password: "4rwur", name: "Jean Hitler", role: "leader" },
-    { id: "id-111-222-33", password: "abc123", name: "Mu2asses", role: "majless" },
-    { id: "id-222-333-44", password: "pass1", name: "Mounzer", role: "majless" },
+    { id: "id-222-333-44", password: "pass1", name: "Monzer", role: "majless" },
     { id: "id-333-444-55", password: "pass2", name: "Hisham", role: "majless" },
     { id: "id-444-555-66", password: "pass3", name: "Adam", role: "majless" },
     { id: "id-555-666-77", password: "pass4", name: "عضو عادي 1", role: "member" },
