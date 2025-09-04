d8828f6e11168ac9afb767604ffd75ab06544dfc
wrrui <97753526+wrrui@users.noreply.github.com>
2025-09-04 20:23:42 +0300

    Add files via upload

---
+++ members.js
@@ -0,0 +1,11 @@
+// members.js
+// List of all members with id, password, name, and role
+const membersData = [
+    { id: "id-120-444-55", password: "4rwur", name: "Jean Hitler", role: "leader" },
+    { id: "id-111-222-33", password: "abc123", name: "Mu2asses", role: "majless" },
+    { id: "id-222-333-44", password: "pass1", name: "Mounzer", role: "majless" },
+    { id: "id-333-444-55", password: "pass2", name: "Hisham", role: "majless" },
+    { id: "id-444-555-66", password: "pass3", name: "Adam", role: "majless" },
+    { id: "id-555-666-77", password: "pass4", name: "عضو عادي 1", role: "member" },
+    { id: "id-666-777-88", password: "pass5", name: "عضو عادي 2", role: "member" }
+];
